9eef7f0bb374543fd058b818504134b75d3c0073
boshrakh95 <boshra.khajehpiri.1@ens.etsmtl.ca>
2026-09-09 23:51:02 -0700

    Added the phase1 jsonl status files

--- jobs/train_mantis_context_sweep_gpu.sh
+++ jobs/train_mantis_context_sweep_gpu.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_1g.10gb:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32000M
-#SBATCH --exclude=fc11006,fc11013,fc11010
+#SBATCH --exclude=fc11006,fc11013,fc11010,fc10713
 #SBATCH --signal=B:USR1@120            # send SIGUSR1 to bash 120s before wall time
 #SBATCH --output=/home/boshra95/NSRR-tools-mantis/logs_mantis/%x_%j.out
 #SBATCH --error=/home/boshra95/NSRR-tools-mantis/logs_mantis/%x_%j.err
